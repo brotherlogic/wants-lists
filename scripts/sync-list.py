258ed56e82ed6b8311cce350122a389a12e19a68
Simon Tucker <s.tucker@dcs.shef.ac.uk>
2009-11-27 23:22:07 +0000

    Updated the script files

--- scripts/sync-list.py
+++ scripts/sync-list.py
@@ -1,6 +1,4 @@
-import pgdb,sys
-
-filename = sys.argv[1]
+import pgdb,sys,os
 
 #Get the connection details
 (username,pwd) = open('details.txt','r').readlines()[0].split(" ")
@@ -9,20 +7,28 @@ filename = sys.argv[1]
 db = pgdb.connect(dsn='192.168.1.103:music',user=username,password=pwd)
 cursor = db.cursor()
 
-for line in open(filename,'r').readlines()[1:]:
-    elems = line.strip().split('~')
-    artist = ""
-    title = ""
-    if len(elems) == 2:
-        (artist,title) = elems
+def sync(filename):
+    for line in open(filename,'r').readlines()[1:]:
+        elems = line.strip().split('~')
+        artist = ""
+        title = ""
+        if len(elems) == 2:
+            (artist,title) = elems
 
-    cursor.execute("select recordnumber from records where author = '" + pgdb.escape_string(artist) + "' AND title = '" + pgdb.escape_string(title) + "'")
-    row = cursor.fetchone()
-    seen = False
-    while row != None:
-        if not seen:
-            print artist,title
-            seen = True
-
-        print "\t",row[0]
+        cursor.execute("select recordnumber from records where author = '" + pgdb.escape_string(artist) + "' AND title = '" + pgdb.escape_string(title) + "'")
         row = cursor.fetchone()
+        seen = False
+        while row != None:
+            if not seen:
+                print artist,title
+                seen = True
+
+            print "\t",row[0]
+            row = cursor.fetchone()
+
+if len(sys.argv) > 1:
+    sync(sys.argv[1])
+else:
+    for file in os.listdir('.'):
+        if file.endswith('.list'):
+            sync(file)
